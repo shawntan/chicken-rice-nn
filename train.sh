84090c4b87471575e51efb3c8c30be106474a829
Shawn Tan <shawn@wtf.sg>
2015-05-27 03:15:50 +0800

    Formatted help. Added README.md

--- train.sh
+++ train.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-THEANO_FLAGS=device=gpu1 python2 -i -u train.py ~/edmw_data/dataset.txt vocab.pkl > train.log &
-
-tail -f train.log
+THEANO_FLAGS=device=gpu1 python2 -i -u train.py ~/edmw_data/dataset.txt vocab.pkl
