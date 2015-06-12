10ed3863f01ca4df30c0abc9962847085bd0820b
Shawn Tan <shawn@wtf.sg>
2015-06-12 16:22:29 +0800

    Moving to after __main__

--- vocab.py
+++ vocab.py
@@ -1,22 +1,6 @@
 import argparse
 import cPickle as pickle
 import sys
-parser = argparse.ArgumentParser(
-    description='Extract unique characters in dataset.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
-parser.add_argument(
-    'data_file',
-    type=argparse.FileType('r'),
-    help="Text file for training."
-)
-parser.add_argument(
-    'vocab_file',
-    type=argparse.FileType('wb'),
-    help="Output file to be used by train.py."
-)
-args = parser.parse_args()
-
 
 def load(filename):
     data = pickle.load(open(filename))
@@ -24,7 +8,25 @@ def load(filename):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Extract unique characters in dataset.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'data_file',
+        type=argparse.FileType('r'),
+        help="Text file for training."
+    )
+    parser.add_argument(
+        'vocab_file',
+        type=argparse.FileType('wb'),
+        help="Output file to be used by train.py."
+    )
+    args = parser.parse_args()
+
+
     charset = set()
     for line in args.data_file:
         charset.update(line)
     pickle.dump(sorted(charset), args.vocab_file, 2)
+    args.vocab_file.close()
